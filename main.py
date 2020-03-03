f60a91ca51d14a02f752465952e76e85ede11f0b
littlefish12345 <18819303050@163.com>
2020-03-03 09:32:19 +0800

    改了下数据结构，可以用了

--- main.py
+++ main.py
@@ -12,35 +12,48 @@ def read_html(name):
 
 app = flask.Flask(__name__)
 
-@app.route('/assets',defaults={'path':''}) #处理assets
-@app.route('/assets/<path:path>')
-def get_assets_file(path):
-    assets_name = ''.join(path.split('..')) #稍微预防一下
-    assets_list = assets_name.split('/')
-    assets_path = os.path.join(os.getcwd(),'html','assets')
-    for strs in assets_list:
-        assets_path = os.path.join(assets_path,strs)
-    if os.path.isfile(assets_path): #有文件就返回文件内容
-        f = open(assets_path,'rb')
-        assets_file = f.read()
+@app.route('/css/<css_name>') #css文件处理
+def get_css_file(css_name):
+    css_name = ''.join(css_name.split('..')) #稍微预防一下
+    css_name = ''.join(css_name.split('/'))
+    css_name = ''.join(css_name.split('\\'))
+    print(os.path.join(os.getcwd(),'html','css',css_name))
+    if os.path.isfile(os.path.join(os.getcwd(),'html','css',css_name)): #有文件就返回文件内容
+        f = open(os.path.join(os.getcwd(),'html','css',css_name),'r')
+        css_file = f.read()
         f.close()
-        return flask.Response(assets_file,mimetype='text/css')
+        return flask.Response(css_file,mimetype='text/css')
     else:
         flask.abort(404) #没文件就返回404
 
-@app.route('/images',defaults={'path':''}) #处理images
-@app.route('/images/<path:path>')
-def get_images_file(path):
-    images_name = ''.join(path.split('..')) #稍微预防一下
-    images_list = images_name.split('/')
-    images_path = os.path.join(os.getcwd(),'html','images')
-    for strs in images_list:
-        images_path = os.path.join(images_path,strs)
-    if os.path.isfile(images_path): #有文件就返回文件内容
-        f = open(images_path,'rb')
-        images_file = f.read()
+@app.route('/js/<js_name>') #js文件处理
+def get_js_file(js_name):
+    js_name = ''.join(js_name.split('..')) #稍微预防一下
+    js_name = ''.join(js_name.split('/'))
+    js_name = ''.join(js_name.split('\\'))
+    print(os.path.join(os.getcwd(),'html','js',js_name))
+    if os.path.isfile(os.path.join(os.getcwd(),'html','js',js_name)): #有文件就返回文件内容
+        f = open(os.path.join(os.getcwd(),'html','js',js_name),'r')
+        js_file = f.read()
         f.close()
-        return images_file
+        return flask.Response(js_file,mimetype='text/js')
+    else:
+        flask.abort(404) #没文件就返回404
+
+@app.route('/static/<path>') #处理static
+def get_static_file(path):
+    print(path)
+    static_name = ''.join(path.split('..')) #稍微预防一下
+    static_list = static_name.split('/')
+    static_path = os.path.join(os.getcwd(),'html','static')
+    for strs in static_list:
+        static_path = os.path.join(static_path,strs)
+    print(static_path)
+    if os.path.isfile(static_path): #有文件就返回文件内容
+        f = open(static_path,'rb')
+        static_file = f.read()
+        f.close()
+        return static_file
     else:
         flask.abort(404) #没文件就返回404
 
